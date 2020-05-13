25fb6e3ddd9b8cce3901508f12a945eaadbb4181
Nikolay Grebenshikov <grebenshikov.n@gmail.com>
2020-05-13 11:01:45 +0700

    Use FileProvider to share files on Android

--- java/ShareEx/project/android/shareex/ShareEx.java
+++ java/ShareEx/project/android/shareex/ShareEx.java
@@ -4,17 +4,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 import org.haxe.extension.Extension;
+import java.io.File;
 
 public class ShareEx {
 
 	public static void share(String text, String subject, String html, String email, String image, String file, String fileType) {
-		Log.d("mulinella-share", text);
-		Log.d("mulinella-share", subject);
-		Log.d("mulinella-share", html);
-		Log.d("mulinella-share", email);
-		Log.d("mulinella-share", image);
-		Log.d("mulinella-share", file);
-		Log.d("mulinella-share", fileType);
+		Log.d("haxe-share", text);
+		Log.d("haxe-share", subject);
+		Log.d("haxe-share", html);
+		Log.d("haxe-share", email);
+		Log.d("haxe-share", image);
+		Log.d("haxe-share", file);
+		Log.d("haxe-share", fileType);
 		Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
 		sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if ( image.equals("") && file.equals("")) {
@@ -25,7 +26,16 @@ public class ShareEx {
             sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         } else {
 			sendIntent.setType(fileType);
-			sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + file));
+			Uri uri = android.support.v4.content.FileProvider.getUriForFile(
+					Extension.mainContext,
+					Extension.mainContext.getApplicationContext().getPackageName() + ".provider",
+					new File(file));
+			Extension.mainContext.grantUriPermission(
+					Extension.mainContext.getApplicationContext().getPackageName(),
+					uri,
+					Intent.FLAG_GRANT_READ_URI_PERMISSION);
+			Log.d("haxe-share", uri.toString());
+			sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
 			sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 		}
 
